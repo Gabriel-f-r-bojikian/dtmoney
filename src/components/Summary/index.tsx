f9860367f21ca752f4f6202274e14e199ef35d43
Gabriel Fernandes Rosa Bojikian <gabriel.f.r.bojikian@gmail.com>
2021-03-17 11:08:49 -0300

    First commit

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,76 @@
+import { useTransactions } from '../../hooks/useTransactions';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+import { Container } from './styles'
+
+export function Summary() {
+    const { transactions } = useTransactions();
+
+    // const totalDeposit = transactions.reduce((acc, transaction) => {
+    //     if (transaction.type === 'deposit') {
+    //         return acc + transaction.amount;
+    //     }
+
+    //     return acc;
+    // }, 0);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'deposit') {
+            acc.deposits += transaction.amount;
+            acc.total += transaction.amount;
+        } else {
+            acc.withdraws += transaction.amount;
+            acc.total -= transaction.amount;
+        }
+
+        return acc;
+    }, {
+        deposits: 0,
+        withdraws: 0,
+        total: 0,
+    });
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <img src={incomeImg} alt="Entradas"/>
+                    <p>Entradas</p>
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <img src={outcomeImg} alt="Entradas"/>
+                    <p>Saídas</p>
+                </header>
+                <strong>- 
+                    {new Intl.NumberFormat('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdraws)}
+                </strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <img src={totalImg} alt="Entradas"/>
+                    <p>Total</p>
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR',{
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+                </strong>
+            </div>
+        </Container>
+    )
+}
